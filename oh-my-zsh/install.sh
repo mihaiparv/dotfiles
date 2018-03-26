a56749b4c1000f66a91f712c02ab55a54bfbae40
Mihai Parv <mihai.parv@iquestgroup.com>
2018-03-26 23:42:45 +0300

    Initial versions of customizations

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Oh My Zsh is an open source, community-driven framework for managing your zsh configuration.
+#
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
