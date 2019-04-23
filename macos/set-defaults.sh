42c19588ec963a0ae975cbb69c21e73b928c9427
Mihai Parv <Mihai.Parv@iquestgroup.com>
2019-04-23 10:27:50 +0300

    Merged / restructured install scripts

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -440,9 +440,6 @@ defaults write com.apple.terminal StringEncodings -array 4
 #defaults write com.apple.terminal FocusFollowsMouse -bool true
 #defaults write org.x.X11 wm_ffm -bool true
 
-# Install the Solarized Dark theme for iTerm
-open "${HOME}/.dotfiles/init/Solarized Dark.itermcolors"
-
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
