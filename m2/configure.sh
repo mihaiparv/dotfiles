06eec55c8241479559d959e696a3de5b8b64d5b8
Mihai Parv <mihai.parv@cj003763mc.esp.local>
2019-05-02 18:05:41 +0300

    Updated m2 security to be configured from lpass

--- m2/configure.sh
+++ m2/configure.sh
@@ -11,7 +11,7 @@ configure () {
   mkdir -p ~/.m2
   local overwrite_all=false backup_all=false skip_all=false
   link_file $DOTFILES_ROOT/preferences/symlink/m2/settings.xml ~/.m2/settings.xml
-  link_file $DOTFILES_ROOT/preferences/symlink/m2/settings-security.xml ~/.m2/settings.xml
+  read_from_lastpass_to_file "iQuest\personal\m2/settings-security.xml" "--notes" ~/.m2/settings-security.xml "u=rw,go="
 }
 
 info '› Configuring maven settings'
