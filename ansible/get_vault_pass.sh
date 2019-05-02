ca432c8faab7dbd62b414c269707f0e30630fdce
Mihai Parv <mihai.parv@gmail.com>
2019-05-02 20:15:58 +0300

    Added support to read vault pass from Lastpass

--- ansible/get_vault_pass.sh
+++ ansible/get_vault_pass.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 #
-# Retrieves the ansible vault password from the OS X Keychain
+# Retrieves the ansible vault password from various secret stores
 
 read_ansible_vault_pass_from_keychain () {
   security find-generic-password -s ${ANSIBLE_VAULT_PASS_KEYCHAIN_SERVICE:-"ansible.vault.iq"} -w
 }
 
-read_ansible_vault_pass_from_keychain
+read_ansible_vault_pass_from_lastpass () {
+  lpass show "${ANSIBLE_VAULT_PASS_LPASS_ENTRY:-"Shared-IQ-CI/Ansible Vault Passwords/iqdvop vault"}" "${ANSIBLE_VAULT_PASS_LPASS_FIELD:-"--note"}" -q
+}
+
+read_ansible_vault_pass_from_lastpass || read_ansible_vault_pass_from_keychain
