0fe3e8c0b4d55ba2de3084374e77501eabb6d7d3
Mihai Parv <mihai.parv@gmail.com>
2019-05-14 15:02:58 +0300

    Restored public key as well to be used by ssh-copy-id

--- ssh/configure.sh
+++ ssh/configure.sh
@@ -11,6 +11,7 @@ configure () {
   mkdir -p ~/.ssh
   read_from_lastpass_to_file "iQuest\personal\ssh/config" "--notes" ~/.ssh/config "u=rw,go=r"
   read_from_lastpass_to_file "iQuest\personal\ssh/mihai.parv@iquestgroup.com" "--field=Private Key" ~/.ssh/mihai.parv@iquestgroup.com "u=rw,go="
+  read_from_lastpass_to_file "iQuest\personal\ssh/mihai.parv@iquestgroup.com" "--field=Public Key" ~/.ssh/mihai.parv@iquestgroup.com.pub "u=rw,go="
   read_from_lastpass_to_file "personal\ssh/mihai.parv@gmail.com" "--field=Private Key" ~/.ssh/mihai.parv@gmail.com "u=rw,go="
   read_from_lastpass_to_file "personal\ssh/mihai.parv@yahoo.com" "--field=Private Key" ~/.ssh/mihai.parv@yahoo.com "u=rw,go="
 }
