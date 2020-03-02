b68be02f209c0198d6c41a3807696c6b39f6229f
Mihai Parv <mihai.parv@gmail.com>
2020-03-02 21:27:56 +0200

    Updated ssh config
    - replaced iQ key with ANt key
    - restored public keys as well

--- ssh/configure.sh
+++ ssh/configure.sh
@@ -10,10 +10,12 @@ DOTFILES_ROOT=$(pwd -P)
 configure () {
   mkdir -p ~/.ssh
   read_from_lastpass_to_file "iQuest\personal\ssh/config" "--notes" ~/.ssh/config "u=rw,go=r"
-  read_from_lastpass_to_file "iQuest\personal\ssh/mihai.parv@iquestgroup.com" "--field=Private Key" ~/.ssh/mihai.parv@iquestgroup.com "u=rw,go="
-  read_from_lastpass_to_file "iQuest\personal\ssh/mihai.parv@iquestgroup.com" "--field=Public Key" ~/.ssh/mihai.parv@iquestgroup.com.pub "u=rw,go="
+  read_from_lastpass_to_file "iQuest\personal\ssh/mihai.parv@agilenetworks.tech" "--field=Private Key" ~/.ssh/mihai.parv@agilenetworks.tech "u=rw,go="
+  read_from_lastpass_to_file "iQuest\personal\ssh/mihai.parv@agilenetworks.tech" "--field=Public Key" ~/.ssh/mihai.parv@agilenetworks.tech.pub "u=rw,go="
   read_from_lastpass_to_file "personal\ssh/mihai.parv@gmail.com" "--field=Private Key" ~/.ssh/mihai.parv@gmail.com "u=rw,go="
+  read_from_lastpass_to_file "personal\ssh/mihai.parv@gmail.com" "--field=Public Key" ~/.ssh/mihai.parv@gmail.com.pub "u=rw,go="
   read_from_lastpass_to_file "personal\ssh/mihai.parv@yahoo.com" "--field=Private Key" ~/.ssh/mihai.parv@yahoo.com "u=rw,go="
+  read_from_lastpass_to_file "personal\ssh/mihai.parv@yahoo.com" "--field=Public Key" ~/.ssh/mihai.parv@yahoo.com.pub "u=rw,go="
 }
 
 info '› Configuring ssh'
